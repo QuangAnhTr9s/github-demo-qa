875ac91a593090cb979229b80e2d60f8efa7f16d
QuangAnhTr9s <trinhquanganh210799@gmail.com>
2022-09-12 22:57:18 +0700

    Update files

---
+++ cacBuoc.dart
@@ -0,0 +1,14 @@
+//git init de tao git
+//git status de kiem tra
+// git add testGithub.dart de them file testGithub.dart
+//git commit -m "Add testGithub.dart file" de comit va thong bao
+// git branch -M main
+// 
+// git remote add origin https://github.com/QuangAnhTr9s/githhub-demo-qa.git 
+// de dua vao cai repos tren gitgub
+//git push -u origin main de push len
+
+// doi voi cac file tiep theo:
+//git add . de dua tat ca cac file len
+//git commit -m "Add all files" de commit va thong bao
+// git push -u origin main de push len
