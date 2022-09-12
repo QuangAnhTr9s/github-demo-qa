1bcb1f0f1bae2e4a1e4cb80dc7ecf1ef8b48aee6
QuangAnhTr9s <trinhquanganh210799@gmail.com>
2022-09-12 22:19:30 +0700

    Add all files

---
+++ cacBuocDuaLenGithub.dart
@@ -0,0 +1,9 @@
+//git init de tao git
+//git status de kiem tra
+// git add testGithub.dart de them file testGithub.dart
+//git commit -m "Add testGithub.dart file" de comit
+// git branch -M main
+// 
+// remote add origin https://github.com/QuangAnhTr9s/githhub-demo-qa.git 
+// de dua vao cai repos tren gitgub
+//push -u origin main de push len
